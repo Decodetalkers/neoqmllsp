aa6c0f890c5a062c05d06250f2432fcfbd33d226
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2022-09-17 13:32:07 +0800

    update

--- settings.lua
+++ settings.lua
@@ -1,19 +1,21 @@
-local configs = require("lspconfig.configs")
+--local configs = require("lspconfig.configs")
 local nvim_lsp = require("lspconfig")
-if not configs.neoqml then
-    configs.neoqml = {
-        default_config = {
-            --cmd = { "./target/debug/qmllsp" },
-            cmd = { 'nc','localhost', '9257' },
-            filetypes = { "qmljs" },
-            root_dir = function(fname)
-                return nvim_lsp.util.find_git_ancestor(fname)
-            end,
-            on_attach = function(client, bufnr)
-                vim.notify("Lsp Start")
-                require("cmps.cmp_onattach")(client, bufnr)
-            end
+
+return {
+    lsp = {
+        neoqlm = {
+            default_config = {
+                --cmd = { "./target/debug/qmllsp" },
+                cmd = { 'nc', 'localhost', '9257' },
+                filetypes = { "qmljs" },
+                root_dir = function(fname)
+                    return nvim_lsp.util.find_git_ancestor(fname)
+                end,
+                on_attach = function(client, bufnr)
+                    vim.notify("Lsp Start")
+                    require("cmps.cmp_onattach")(client, bufnr)
+                end
+            }
         }
     }
-    nvim_lsp.neoqml.setup({})
-end
+}
